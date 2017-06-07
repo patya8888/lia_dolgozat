02b911481d9275b75b5fdd9096498dde25236024
patya8888 <janigakrisztian18@gmail.com>
2017-06-07 13:41:23 +0200

    Add files via upload

---
+++ orai.js
@@ -0,0 +1,51 @@
+/* jshint esnext: true */
+/* jshint devel: true */
+/* jshint node: true */
+/* jshint browser: true*/
+/* jshint jquery: true*/
+console.log();
+
+let a=7;
+let b=8;
+let c;
+
+console.log("Csere elött A értéke: "+a+" , B értéke: " +b);
+
+let a2 = b;
+let b2 = a;
+
+console.log("lol");
+
+
+
+
+//-----------------------
+
+console.log("");
+console.log("3. feladat");
+
+x = [5, 6, 4, 1, 3, 9] ;
+let van = false;
+let i = 0;
+while (i < x.length && x[i] !== 3) {
+i = i +1;
+}
+if (i <= x.length)
+{
+	van = true;
+}
+console.log ("yes: "+van);
+
+
+///-------
+
+console.log("5.feladat");
+
+let db = 0;
+for (i = 0; i < x.length; i++);
+{
+
+if = x[i] === 2 ); {
+	
+}
+}
